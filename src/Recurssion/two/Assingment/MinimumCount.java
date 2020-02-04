554a9d23fdee2869d49ca597196fb85e3373a99c
kshvkantg <kshvkantg@gmail.com>
2020-02-04 09:13:40 +0530

    -

--- src/Recurssion/two/Assingment/MinimumCount.java
+++ src/Recurssion/two/Assingment/MinimumCount.java
@@ -1,22 +1,34 @@
 package Recurssion.two.Assingment;
-
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class MinimumCount {
     public static int minimumCount(int n){
+        if (n <= 3)
+            return n;
+        int dp[] = new int[n + 1];
 
-        return minimumCount(n,1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
 
-    }
-    public static int minimumCount(int n, int square, int count){
-        if(n <= 1 ){
-            count = count + 1;
-            return count;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = i;
+
+            for (int x = 1; x <= Math.ceil(Math.sqrt(i)); x++) {
+                int temp = x * x;
+                if (temp > i)
+                    break;
+                else
+                    dp[i] = Math.min(dp[i], 1 + dp[i - temp]);
+            }
         }
+        int res = dp[n];
 
-        return minimumCount(n - (int)(Math.pow(square,2)),square + 1,count + 1);
+        return res;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
